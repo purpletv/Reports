6c9198076e59bd1241e165a7a3eb20b5f9223854
purpletv <81405384+purpletv@users.noreply.github.com>
2023-06-25 12:32:50 +0530

    Add files via upload

--- ReportController.java
+++ ReportController.java
@@ -9,9 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import eStoreProduct.DAO.CategoryReportDAO;
 import eStoreProduct.DAO.OrderDAO;
 import eStoreProduct.DAO.ProductDAO;
 import eStoreProduct.model.Category;
+import eStoreProduct.model.CategoryReportViewModel;
 import eStoreProduct.model.orderModel;
 
 @Controller
@@ -20,10 +22,12 @@ public class ReportController {
     
  
     private OrderDAO odao;
+    private CategoryReportDAO crd;
     
     @Autowired
-	public ReportController(   @Qualifier("orderDAOImp") OrderDAO odao) {
+	public ReportController(   @Qualifier("orderDAOImp") OrderDAO odao,CategoryReportDAO crd) {
 	this.odao = odao;
+	this.crd=crd;
 	}
     
 	@GetMapping("/generateGSTReport")
@@ -32,6 +36,17 @@ public class ReportController {
 	    model.addAttribute("orderReport", om);
 	    return "Reports";
 	}
-
-
+	@GetMapping("/categoryReport")
+	public String categoryReport(Model model) {
+	    //List<orderModel> om=odao.getAllOrders();
+	    List<CategoryReportViewModel> cl=crd.getCatRep();
+	    model.addAttribute("categoryReport", cl);
+	    return "catReports";
+	}
+	/*
+	 * @GetMapping("/categoryReportSort") public String categoryReportSort(Model
+	 * model) { //List<orderModel> om=odao.getAllOrders();
+	 * List<CategoryReportViewModel> cl= model.addAttribute("categoryReport", cl);
+	 * return "catReports"; }
+	 */
 }
